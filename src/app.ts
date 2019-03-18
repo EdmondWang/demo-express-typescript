fc78b1e78a06d07b8753ea73e6420ed651b7e478
Edmond Wang <ywang53@ebay.com>
2019-03-18 18:10:26 +0800

    add ejs and shelljs

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 const port = 3000; // default port to listen
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 // define a route handler for the default home page
 app.get("/", (req, res) => {
-    res.send("Hello world!");
+    res.render("index");
 });
 
 // start the Express server
